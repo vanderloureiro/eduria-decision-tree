9f214d4d9750aeca296a1e7fdad7da958585dd37
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2020-09-17 17:37:11 -0300

    Change port and more complex search

--- src/main/java/com/br/eduriadecisiontree/domain/DecisionTree.java
+++ src/main/java/com/br/eduriadecisiontree/domain/DecisionTree.java
@@ -11,17 +11,24 @@ public class DecisionTree {
     public void createTree() {
         Node n1 = new ScoreNode(p -> p < 4);
         Node n2 = new ScoreNode(p -> p < 7);
-        Node n3 = new FinalNode(LevelEnum.EASY);
-        Node n4 = new FinalNode(LevelEnum.MEDIUM);
-        Node n5 = new FinalNode(LevelEnum.HARD);
+        Node n3 = new QttAnsweredNode(p -> p < 20);
+        Node n4 = new QttAnsweredNode(p -> p % 2 != 0); //odd
+        Node n5 = new FinalNode(LevelEnum.EASY);
+        Node n6 = new FinalNode(LevelEnum.MEDIUM);
+        Node n7 = new FinalNode(LevelEnum.HARD);
+        Node n8 = new FinalNode(LevelEnum.RANDOM);
 
-        // setting first level
-        n1.setLeftNode(n3);
-        n1.setRightNode(n2);
+        n1.leftNode  = n3;
+        n1.rightNode = n2;
 
-        // setting secong level
-        n2.setLeftNode(n4);
-        n2.setRightNode(n5);
+        n2.leftNode  = n6;
+        n2.rightNode = n7;
+
+        n3.leftNode  = n4;
+        n3.rightNode = n5;
+
+        n4.leftNode  = n8;
+        n4.rightNode = n5;
 
         this.root = n1;
     }
